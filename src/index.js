877c830d3e03586407e833af5d9d731f5c15f8d1
voiceinthedark <darkrisingforce@gmail.com>
2025-07-28 21:14:36 +0300

    weatherjs: add basic architecture

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-import Dropdowner from "./modules/dropdowner";
 import "./styles/styles.css";
 import "./styles/fontawesome.min.css";
 import "./styles/regular.min.css";
 import "./styles/solid.min.css";
 import "./webfonts/fa-solid-900.ttf";
+
+const appContainer = document.getElementById("container");
+
+fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Chehour?unitGroup=metric&key=7WXVPBFAC4BMBGSURKG4AJBTP&contentType=json", {
+  "method": "GET",
+  "headers": {
+  }
+})
+  .then(response => {
+    return response.json()
+  })
+  .then(res => console.log(res))
+  .catch(err => {
+    console.error(err);
+  });
 
---
+++ src/modules/todayWeather.js
@@ -0,0 +1,103 @@
+// @ts-check
+
+/**
+ * @class TodayWeather
+ * @description Holds information about weather condition for any given day
+ * */
+class TodayWeather{
+  #address;
+  #resolvedAddress;
+  #datetime;
+  #temperature;
+  #feelsLike;
+  #humidity;
+  #precipitation;
+  #windspeed;
+  #solarEnergy;
+  #condition;
+  #icon;
+  #sunrise;
+  #sunset;
+
+  /**
+   * @constructor
+   * @param {Object} weatherData 
+   * @param {string} weatherData.address
+   * @param {string} weatherData.resolvedAddress
+   * @param {string} weatherData.datetime
+   * @param {number} weatherData.temperature
+   * @param {number} weatherData.feelsLike
+   * @param {number} weatherData.humidity
+   * @param {number} weatherData.precipitation
+   * @param {number} weatherData.windspeed
+   * @param {number} weatherData.solarEnergy
+   * @param {string} weatherData.condition
+   * @param {string} weatherData.icon
+   * @param {string} weatherData.sunrise
+   * @param {string} weatherData.sunset
+   * */
+  constructor(weatherData){
+    this.#address = weatherData.address;
+    this.#resolvedAddress = weatherData.resolvedAddress;
+    this.#temperature = weatherData.temperature
+    this.#feelsLike = weatherData.feelsLike
+    this.#datetime = weatherData.datetime
+    this.#humidity = weatherData.humidity
+    this.#precipitation = weatherData.precipitation
+    this.#windspeed = weatherData.windspeed
+    this.#solarEnergy = weatherData.solarEnergy
+    this.#condition = weatherData.condition
+    this.#icon = weatherData.icon
+    this.#sunset = weatherData.sunset
+    this.#sunrise = weatherData.sunrise
+  }
+
+  get address(){
+    return this.#address;
+  }
+  get resolvedAddress(){
+    return this.#resolvedAddress
+  }
+  get temperature(){
+    return this.#temperature
+  }
+  get feelsLike(){
+    return this.#feelsLike
+  }
+  get datetime(){
+    return this.#datetime
+  }
+  get humidity(){
+    return this.#humidity
+  }
+  get precipitation(){
+    return this.#precipitation
+  }
+  get windspeed(){
+    return this.#windspeed
+  }
+  get solarEnergy(){
+    return this.#solarEnergy
+  }
+  get condition(){
+    return this.#condition
+  }
+  get sunrise(){
+    return this.#sunrise
+  }
+  get sunset(){
+    return this.#sunset
+  }
+
+  get icon(){
+    return this.#icon
+  }
+  /**
+   * @param {string} val 
+   * */
+  set icon(val){
+    this.#icon = val
+  }
+
+
+}
